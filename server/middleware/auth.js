bd622a8c19373a719a38fc44fa936fbeb876f568
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2024-03-14 12:10:15 +0530

    auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,21 +2,21 @@ const jwt = require('jsonwebtoken')
 const keysecret = 'blogerposttoken';
 
 
-function logauth (req,res,next){
-    const token = req.headers.auth
-    console.log('token',token);
-    if(!token){
-        return res.status(401).json({ message: 'Unauthorized' });
-    }else{
-        {
-            jwt.verify(token, keysecret, (err, decodedToken) => {
-              if (err) return res.status(401).json({ message: 'Token verification failed' });
-              req.userId = decodedToken.userId;
-              next();
-            });
-          }
+    function logauth (req,res,next){
+        const token = req.headers.Authorization
+        console.log('token',token);
+        if(!token){
+            res.status(401).json({ message: 'Unauthorized' });
+        }else{
+            {
+                jwt.verify(token, keysecret, (err, decodedToken) => {
+                if (err) return res.status(401).json({ message: 'Token verification failed' });
+                req.userId = decodedToken.userId;
+                next();
+                });
+            }
+        }
     }
-}
 
 
 
