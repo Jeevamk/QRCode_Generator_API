e827983254114e74ddf7f9eea08a6923a8940146
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2024-03-15 21:44:21 +0530

    assets folder

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const cloudinary = require("cloudinary").v2;
 const multer = require('multer');
-const upload = multer({ dest : 'assests/images'})
+const upload = multer({ dest : 'assets/images'})
 const multipleUpload = upload.array('images', 5);
 
 //cloudinary config
